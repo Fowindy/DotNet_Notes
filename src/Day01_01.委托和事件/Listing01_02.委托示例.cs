3762722fd4f915c8b0c92edfea094cc2b02bda96
Fowindy <47002339@qq.com>
2020-05-30 09:39:28 +0800

    委托案例1完成

---
+++ src/Day01_01.委托和事件/Listing01_02.委托示例.cs
@@ -0,0 +1,55 @@
+﻿/* ==============================================================================
+ * 创 建 者：Fowindy
+ * 创建日期：2020年5月30日 星期六 09:15:52
+ * ==============================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+
+namespace Fowindy.Day01_01.委托和事件.Listing01_02
+{
+    public class Listing01_02
+    {
+        /// <summary>
+        /// 声明一个打招呼的委托函数
+        /// </summary>
+        /// <param name="name"></param>
+        public delegate void GreetingDelegate(string name);
+        /// <summary>
+        /// 声明语言的枚举
+        /// </summary>
+        public enum Language
+        {
+            English,
+            Chinese
+        }
+        /// <summary>
+        /// 声明英语打招呼的方法
+        /// </summary>
+        /// <param name="name"></param>
+        public static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Good Morning,"+ name);
+        }
+        /// <summary>
+        /// 声明中文打招呼的方法
+        /// </summary>
+        /// <param name="name"></param>
+        public static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("早上好,"+ name);
+        }
+        public static void GreetPeople(string name,GreetingDelegate greetingDelegate)
+        {
+            greetingDelegate(name);
+        }
+        public static void Main()
+        {
+            GreetPeople("Mark", EnglishGreeting);
+            GreetPeople("马云", ChineseGreeting);
+            Console.ReadKey();
+        }
+    }
+}
