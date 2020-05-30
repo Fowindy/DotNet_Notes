a145a69eee8673fa2e73ff9985144afddb025129
Fowindy <47002339@qq.com>
2020-05-30 14:28:44 +0800

    委托的修饰符的矛盾点

--- src/Day01_01.委托和事件/GreetingManager.cs
+++ src/Day01_01.委托和事件/GreetingManager.cs
@@ -8,6 +8,12 @@ namespace Day01_01.委托和事件
     {
         //直接在GreetingManager类的内部声明greetingDelegate变量
         public GreetingDelegate greetingDelegate;
+        /************************************************************************/
+        /* 委托的修饰符的矛盾点:
+            1.声明委托的目的就是为了把它暴露在类的客户端进行方法的注册,因此不能使用private
+            2.声明成public又会导致在客户端可以对它进行随意的赋值等操作，严重破坏对象的封装性
+            */
+        /************************************************************************/
         public void GreetPeople(string name)
         {
             if (greetingDelegate != null)   //如果有方法注册委托变量
